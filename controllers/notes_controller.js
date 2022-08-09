b4445e6e2c8a73f1c1514c2926e0fdea28e771cc
cre8ivesolutions <tammerz78@gmail.com>
2022-08-08 21:12:27 -0500

    third

--- controllers/notes_controller.js
+++ controllers/notes_controller.js
@@ -1,11 +1,24 @@
-const express = requrie('express')
+const express = require('express')
 const notes = express.Router()
 const Note = require("../models/notes.js")
-const notesSeedDate = require("../models/notes_seed")
+const notesSeedDate = require("../models/notes_seed.js")
+
+notes.get("/", async (req, res) => {
+    const notes = await Note.find();
+    console.log({notes});
+    res.render ('index', {
+        notes: notes, 
+        title: "To Do App Home"
+    })
+})
 
 notes.get("/data/seed", async (req, res) => {
     const seeded = await Note.insertMany(notesSeedData);
     console.log(seeded)
+})
+
+notes.post('/', (req, res) => {
+    Note.create(req.body)
 })
 
 module.exports = notes;
--- server.js
+++ server.js
@@ -1,47 +1,37 @@
-// DEPENDENCIES
-const express = require('express')
-const methodOverride = require('method-override')
-const mongoose = require('mongoose');
+const express = require("express");
+const app = express();
+const methodOverride = require("method-override");
+require("dotenv").config();
+const mongoose = require("mongoose");
 mongoose.connect(
-    process.env.MONGO_URI, 
-    {useNewUrlParser: true, 
-    useUnifiedTopology: true}
+  process.env.MONGO_URI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  () => {
+    console.log("mongodb connection success");
+  }
 );
+const PORT = process.env.PORT;
 
-// CONFIGURATION
-require('dotenv').config()
-const PORT = process.env.PORT
-const app = express()
-mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
-  () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
-)
+app.use(methodOverride("_method"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.set("view engine", "jsx");
+app.engine("jsx", require("express-react-views").createEngine());
 
-// MIDDLEWARE
-app.use(methodOverride('_method'))
-app.use(express.urlencoded({extended: true}))
-app.use(express.static('public'))
-app.set('view engine', 'jsx')
-app.engine('jsx', require('express-react-views').createEngine())
+app.get("/health", (req, res) => {
+  res.send("server is healthy");
+});
 
-// ROUTES
-app.get('/', (req, res) => {
-  res.send('Welcome to an awesome App about Breads')
-})
+const notesController = require("./controllers/notes_controller");
+app.use("/notes", notesController);
 
-// Breads
-const breadsController = require('./controllers/breads_controller.js')
-app.use('/breads', breadsController)
+app.get("*", (req, res) => {
+  res.send("404");
+});
 
-// Bakers 
-const bakersController = require('./controllers/bakers_controller.js')
-app.use('/bakers', bakersController)
-
-// 404 Page
-app.get('*', (req, res) => {
-  res.send('404')
-})
-
-// LISTEN
 app.listen(PORT, () => {
-  console.log('nomming at port', PORT);
-})
+  console.log("running at port", PORT);
+});
